3abe98110dabbe288b5de0cda80d5550e3991edb
Carlos <c.alvarez@payvision.com>
2018-06-22 12:35:05 +0200

    Contract

---
+++ SubModuleTest/SubModuleTest/Contract.cs
@@ -0,0 +1,13 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Contract.cs" company="Payvision">
+//     Payvision Copyright © 2018
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace SubModuleTest
+{
+    public interface IContract
+    {
+        int ContractNumber();
+    }
+}
